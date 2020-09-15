f3b610f46ae57a76e93d8baf8f98bf2c055ea720
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-09-15 15:33:04 -0700

    Pass root reducer ADD_TICKET test.
    hCo-authored-by: Peter Grimm <pagrimm@gmail.com>
    Co-authored-by: Chris Yoon <chy.yoon@gmail.com>

--- src/__test__/reducers/index-reducer.test.js
+++ src/__test__/reducers/index-reducer.test.js
@@ -13,14 +13,26 @@ describe("rootReducer", () => {
       formVisibleOnPage: false
     });
   });
- 
+  
   test('Check that initial state of postListReducer matches root reducer', () => {
     expect(store.getState().mainPostList).toEqual(postListReducer(undefined, { type: null }));
   });
 
-    test('Check that initial state of formVisibleReducer matches rootReducer', () => {
-      expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
-    });
-
-    
+  test('Check that initial state of formVisibleReducer matches rootReducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
   });
+
+  test('Check that state of postListReducer matches rootReducer', () => {
+    const action = {
+      type: 'ADD_TICKET',
+      name: 'Steve',
+      content: 'Check out this cat meme',
+      score: 0,
+      id: 1,
+      timestamp: Date.now()
+    };
+    store.dispatch(action);
+    expect(store.getState().mainPostList).toEqual(postListReducer(undefined, action));
+  });
+
+});
