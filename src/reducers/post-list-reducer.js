92c3dce1099a06c612f232be21b799d0f81aa153
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-09-15 13:59:55 -0700

    Pass test to add a post.
    Co-authored-by: Chris Yoon <chy.yoon@gmail.com>
    Co-authored-by: Peter Grimm <pagrimm@gmail.com>

--- src/reducers/post-list-reducer.js
+++ src/reducers/post-list-reducer.js
@@ -1,3 +1,17 @@
 export default (state = {}, action) => {
-  return state;
+  const { name, content, score, id, timestamp } = action;
+  switch (action.type){
+    case 'ADD_POST':
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          content: content,
+          score: score,
+          id: id,
+          timestamp: timestamp
+        }
+      });
+    default:
+      return state;
+  }
 };
