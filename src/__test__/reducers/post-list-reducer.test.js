77f640a401263f6d8a5ed962baa60cdad6887dcd
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-09-15 13:52:20 -0700

    Get meaningful fail for ADD_TICKET test.
    Co-authored-by: Chris Yoon <chy.yoon@gmail.com>
    Co-authored-by: Peter Grimm <pagrimm@gmail.com>

--- src/__test__/reducers/post-list-reducer.test.js
+++ src/__test__/reducers/post-list-reducer.test.js
@@ -1,8 +1,36 @@
 import postListReducer from '../../reducers/post-list-reducer';
 
 describe('postListReducer', () => {
-
+  let action;
+  const postData = {
+    name: 'Steve',
+    content: 'Check out this cat meme',
+    score: 0,
+    id: 1,
+    timestamp: Date.now()
+  };
   test('Should return default state if there is no action type passed into reducer', () => {
     expect(postListReducer({}, {type: null})).toEqual({});
+  });
+
+  test('Should successfully add post to mainPostList', () => {
+    const { name, content, score, id, timestamp } = postData;
+    action = {
+      type: 'ADD_POST',
+      name: name,
+      content: content,
+      score: score,
+      id: id,
+      timestamp: timestamp
+    };
+    expect(postListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        content: content,
+        score: score,
+        id: id,
+        timestamp: timestamp
+      }
+    });
   });
 });
